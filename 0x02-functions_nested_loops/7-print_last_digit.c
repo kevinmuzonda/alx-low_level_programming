df0bcbf402432e98059ebfa48539474e7a63d4c8
root <root@Kevin.localdomain>
2023-08-16 21:03:47 +0200

    functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,12 @@ int print_last_digit(int n)
 	x = n % 10;
 	if (x < 0)
 	{
-		_putchar(x + 98);
-			return (x);
+		_putchar(-x + 48);
+			return (-x);
 	}
 	else
 	{
-		_putchar(-x + 98);
-		return (-x);
+		_putchar(x + 48);
+		return (x);
 	}
 }

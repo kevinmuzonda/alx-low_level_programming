d2ed0cd1d4ff5579046ed4f5849e36690089e016
root <root@Kevin.localdomain>
2023-08-16 22:05:25 +0200

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - adds two numbers and returns result
+ *Return:sum of two integers
+ */
+int add(int, int)
+{
+	int a = 89;
+	int b = 9;
+	int  sum;
+
+	sum = a + b;
+	_putchar(sum);
+}
+
+

d8281cab220313ecf53014a6c696542990db3a9a
root <root@Kevin.localdomain>
2023-08-21 22:50:40 +0200

    string_length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _strlen - Function returns the length of a string
+  * @s: points to  a string
+  * Return: Always  (success).
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	do {
+		length++;
+
+	} while (s[length]);
+	return (length);
+}

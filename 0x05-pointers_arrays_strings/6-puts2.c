2983528813757b6cbbb0b4cc7ac347579e5a2d9a
root <root@Kevin.localdomain>
2023-08-23 00:51:39 +0200

    array

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,13 @@ void puts2(char *str)
 {
 	int n = 0;
 
-	do {
+	while (str[n] != '\0');
+	{
 		if (n % 2 == 0)
 		{
 			_putchar(str[n]);
 		}
 		n++;
 	}
-	while (str[n] != '\0');
 	_putchar('\n');
 }

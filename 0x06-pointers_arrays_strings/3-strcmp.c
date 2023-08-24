67182195d32a3407e95ccd71bd2f966857d86584
root <root@Kevin.localdomain>
2023-08-25 00:09:19 +0200

    pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @s1: first string
+ * @s2: second string
+ * Return: Always (success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
+
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int c;
+	int i;
+
+	for (i = 0; i < n--; i++)
+	{
+		c = a[i];
+		a[i] = a[n];
+		a[n] = c;
+	}
+}
+

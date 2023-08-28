2e6279afff7be9e83cb19a21c2f8870569e87539
root <root@Kevin.localdomain>
2023-08-29 00:08:43 +0200

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * chessboard - prints a chessboard pattern
+  * @a: this is an array
+  * Return: Always(success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}

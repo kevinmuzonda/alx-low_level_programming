6ba584f4602770b0805e4e6f1f40e656d5d4db35
root <root@Kevin.localdomain>
2023-08-30 21:57:46 +0200

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * square_root - function that returns a sqaure root
+  * @a: input number.
+  * @b: second input.
+  * Return: always(succes).
+ */
+int square_root(int i, int j)
+{
+	if (i * i > j)
+	{
+		return (-1);
+	}
+	else if (i * i == j)
+	{
+		return (j);
+	}
+	return (square_root(i, j + 1));
+}
+/**
+  * _sqrt_recursion - a function that returns a sqaure root
+  * @n: number to find the square root
+  * Return: Always (success)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (square_root(n, 1));
+}

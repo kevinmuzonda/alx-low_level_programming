b90be47746bd5a0926943739f734abde57c62b94
root <root@Kevin.localdomain>
2023-09-03 22:28:44 +0200

    multi_num

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: contains integer
+ * @argv: contains string variables
+ * @a: int value
+ * @b: int value
+ * Return: always(success)
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a * b;
+	printf("%d\n", sum);
+	return (0);
+}

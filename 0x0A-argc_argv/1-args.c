66ff3d88383136fd2786cb0e1aa1786e1a3b15bf
root <root@Kevin.localdomain>
2023-09-03 21:40:05 +0200

    Hello_world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - program prints the num of arguments passwed 
+  * @argc: contains integer arguments
+  * @argv: contains string argument
+  * Return: always(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

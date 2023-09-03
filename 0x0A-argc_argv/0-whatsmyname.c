9dc0c3a74f1cc76d5793439bc37b7bd2acc81f66
root <root@Kevin.localdomain>
2023-09-03 20:36:49 +0200

    new_me

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * @argc: takes integer values
+  * @argv: takes in strings
+  * Return: always (success).
+ */
+
+int main(int argc, char* argv[])
+{
+	argc = argc;
+
+	printf("%s\n",*argv);
+	return (0);
+}

c7566f88518d4c68dedc839f9e7ffa27ea0b2037
root <root@Kevin.localdomain>
2023-09-03 22:13:59 +0200

    mission_impossible

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints all arguments it receives
+  * @argc: contains integer arguments
+  * @argv: contains string arguments
+  * Return: always(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}

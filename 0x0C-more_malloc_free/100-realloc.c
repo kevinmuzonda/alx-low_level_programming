0015ac89ee18c9c1135ff4d7e32c2924c5af51f1
root <root@Kevin.localdomain>
2023-09-08 00:56:28 +0200

    more_malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	if (ptr == NULL) 
+	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)

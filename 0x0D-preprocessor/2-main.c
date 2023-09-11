123cbddc88e60ab2b4a2488e3478e423ee9f2f3c
root <root@Kevin.localdomain>
2023-09-12 00:08:25 +0200

    preprocess

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
   * main - a program that prints the name of a file it was compiled from
-  *  
+  *
   * Return: Always(success)
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }

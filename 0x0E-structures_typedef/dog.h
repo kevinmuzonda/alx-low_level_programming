508f8432b5be00dca76ef67fc9757047564dd727
root <root@Kevin.localdomain>
2023-09-12 23:16:13 +0200

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+  * struct dog - function that shows the dog and owners information
+  * @name: the dogs name
+  * @age: the dogs age
+  * @owner: the owner of the dog
+  * Return: Always(success)
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+/**
+  * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);

998c681f85acab7a1f432268e1131982ff826372
root <root@Kevin.localdomain>
2023-09-12 23:19:29 +0200

    Structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 	}
 	else if (d->name == NULL)
 	{
-		d->name = ("nil");
+		d->name = "(nil)";
 	}
 	else if (d->owner == NULL)
 	{
-		d->owner = ("nil");
+		d->owner = "(nil)";
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

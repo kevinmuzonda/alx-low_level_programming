02da67277cd3fb13a3dd035fcdcb9fa2a5e109dd
Samswagz <ilesamuel53@gmail.com>
2023-09-21 22:44:54 +0200

    elemental

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,23 +2,22 @@
 
 /**
   * print_list - prints all elements of a list_t list
-  * @h: the lists 
+  * @h: the lists
   *Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t a;
-	
-	for (a = 0; h != NULL; h = h -> next, a++)
+
+	for (a = 0; h != NULL; h = h->next, a++)
 	{
-		
-		if (h -> str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h -> len, h -> str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 	}
 	return (a);

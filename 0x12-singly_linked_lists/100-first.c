011d28ae82b95fb15d2d086c5c051da16c5a5ee0
Samswagz <ilesamuel53@gmail.com>
2023-09-23 01:00:46 +0200

    Tortoise_race

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * tortoise - code that prints before the main function
+  *
+  * Return: always(Success)
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

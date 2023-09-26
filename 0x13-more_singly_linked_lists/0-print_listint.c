86c07933e05909774a25b50ae9fdccfdf037ed92
Samswagz <ilesamuel53@gmail.com>
2023-09-27 01:04:45 +0200

    nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,11 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t a;
+	size_t a = 0;
 
-	for (a = 0; h != NULL; h = h->next, a++)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		a++;
+		h = h->next;
 	}
 	return (a);
 }

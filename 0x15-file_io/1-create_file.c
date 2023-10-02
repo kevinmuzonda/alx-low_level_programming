3fc00914f9685061fb1542c37a52a81d9c5cd147
Samswagz <ilesamuel53@gmail.com>
2023-10-02 23:04:47 +0200

    Append

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (letters = 0; text_content[letters]; letters++);
+	for (letters = 0; text_content[letters]; letters++)
+		;
 
 	read = write(file, text_content, letters);
 

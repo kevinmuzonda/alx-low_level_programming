90b8ab01a9ef4d7b5471357fffb75eda27b51b0b
Samswagz <ilesamuel53@gmail.com>
2023-10-02 22:51:41 +0200

    Creation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,33 +8,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int letters;
-	int read;
+	int file, letters, read;
 
 	if (!filename)
-	{
 		return (-1);
-	}
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
-	{
 		return (-1);
-	}
+
 	if (!text_content)
-	{
 		text_content = "";
-	}
-	for (letters = 0; text_content[letters]; letters++)
-	{
-		read = write(file, text_content, letters);
-	}
+
+	for (letters = 0; text_content[letters]; letters++);
+
+	read = write(file, text_content, letters);
 
 	if (read == -1)
-	{
 		return (-1);
-	}
 	close(file);
 
 	return (1);
